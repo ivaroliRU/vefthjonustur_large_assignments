1c132b37d2b462eec6a854b116b1f1516781fbed
Ivar Oli <ivars17@ru.is>
2019-09-05 12:18:26 +0000

    some news example

--- large_assignment1/Program.cs
+++ large_assignment1/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace large_assignment1
+namespace TechnicalRadiation
 {
     public class Program
     {
