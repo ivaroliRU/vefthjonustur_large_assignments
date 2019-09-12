5d6942cce634a748ec8a498fbebbf86b2ddf7f66
arnabjorg17 <34029748+arnabjorg17@users.noreply.github.com>
2019-09-12 11:27:37 +0000

    Data

--- TechnicalRadiation/TechnicalRadiation.Repositories/Data/TechnicalRadiationDbContext.cs
+++ TechnicalRadiation/TechnicalRadiation.Repositories/Data/TechnicalRadiationDbContext.cs
@@ -12,10 +12,34 @@ namespace TechnicalRadiation.Repositories.Data
         {
             get 
             {
-                using (StreamReader r = new StreamReader("data.json"))
+                using (StreamReader r = new StreamReader("news.json"))
                 {
                     string json = r.ReadToEnd();
                     List<NewsItem> items = JsonConvert.DeserializeObject<List<NewsItem>>(json);
+                    return items;
+                }
+            }
+        }
+        public IEnumerable<Author> Author
+        {
+            get 
+            {
+                using (StreamReader r = new StreamReader("author.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<Author> items = JsonConvert.DeserializeObject<List<Author>>(json);
+                    return items;
+                }
+            }
+        }
+        public IEnumerable<Category> Category
+        {
+            get 
+            {
+                using (StreamReader r = new StreamReader("category.json"))
+                {
+                    string json = r.ReadToEnd();
+                    List<Category> items = JsonConvert.DeserializeObject<List<Category>>(json);
                     return items;
                 }
             }
