44e98893d911d44b7d9bee495aca83cbef0303f6
Ivar Oli <ivars17@ru.is>
2019-09-13 11:37:30 +0000

    authorization

--- TechnicalRadiation/TechnicalRadiation.WebApi/Attributes/AuthorizationAttribute.cs
+++ TechnicalRadiation/TechnicalRadiation.WebApi/Attributes/AuthorizationAttribute.cs
@@ -1,13 +1,25 @@
 using System;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TechnicalRadiation.WebApi.Attributes
 {
     public class AuthorizationAttribute : ActionFilterAttribute
     {
-        public override void OnActionExecuted(ActionExecutedContext context)
+        string authString = "TechnicalRadiation";
+
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
-            Console.WriteLine(context.HttpContext.Connection.LocalIpAddress);
+            string authorizationToken = context.HttpContext.Request.Headers["Authorization"];
+
+            if(authorizationToken == authString){
+                Console.WriteLine("You are in :)");
+            }
+            else{
+                Console.WriteLine("Not authorized >:(");
+                context.Result = new UnauthorizedResult();
+            }
+
             Console.WriteLine(context.HttpContext.Request.Path);
         }
     }
--- TechnicalRadiation/TechnicalRadiation.WebApi/Controllers/NewsItemsController.cs
+++ TechnicalRadiation/TechnicalRadiation.WebApi/Controllers/NewsItemsController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TechnicalRadiation.Models;
 using TechnicalRadiation.Models.DtoModels;
+using TechnicalRadiation.Models.InputModels;
 using TechnicalRadiation.Services.Interfaces;
 using System.Net.Http.Headers;
+using TechnicalRadiation.WebApi.Attributes;
 
 namespace TechnicalRadiation.Controllers
 {
@@ -26,6 +28,18 @@ namespace TechnicalRadiation.Controllers
         public ActionResult<Envelope<NewsItemDto>> GetAllNewsItems([FromQuery] int pageSize = 25, [FromQuery] int pageNumber = 1)
         {
             return Ok(_newsService.GetAllNewsItems(pageSize, pageNumber));
+        }
+
+        // POST api/
+        [HttpPost("")]
+        [Authorization]
+        public IActionResult CreateNewCustomer([FromBody] NewsItemInputModel news)
+        {
+            if (!ModelState.IsValid) { return StatusCode(412, news); }
+
+            var id = 0;//do some stuff
+
+            return Ok();//CreatedAtRoute("GetNewsItemsById", new { id }, null);
         }
 
         // GET api/{newsItemId}
