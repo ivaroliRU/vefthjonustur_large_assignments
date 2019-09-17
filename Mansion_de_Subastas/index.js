9f551ad7c3d308a3fa55310d1d9b1ce7ab208d70
Ivar Oli <ivars17@ru.is>
2019-09-17 11:56:46 +0000

    file structure for project

---
+++ Mansion_de_Subastas/index.js
@@ -0,0 +1,17 @@
+/*
+mansion_de_subastas/
+• schemas/
+    • art.js
+    • artist.js
+    • auction.js
+    • auctionBid.js
+    • customer.js
+• data/
+    • db.js
+• services/
+    • artService.js
+    • artistService.js
+    • auctionService.js
+    • customerService.js
+index.js
+*/
